4d540f025123547c0fd1f358a21297fc04392b0b
Micah Smith <micahjsmith@gmail.com>
2016-02-02 11:25:02 -0500

    Add Flex theme as submodule

--- pelicanconf.py
+++ pelicanconf.py
@@ -16,7 +16,7 @@ TIMEZONE = "America/New_York"
 DEFAULT_LANG = "en"
 
 # Theme
-THEME = "flex"
+THEME = "themes/Flex/"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
