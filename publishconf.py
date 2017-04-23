529dcfd562d9aad3722e64272825ce004825d84e
Micah Smith <micahjsmith@gmail.com>
2017-04-23 11:49:32 -0400

    Add www

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = "http://micahsmith.com"
+SITEURL = "http://www.micahsmith.com"
 SITELOGO = SITEURL + "/images/micah.jpg"
 RELATIVE_URLS = False
 
