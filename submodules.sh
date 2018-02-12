6d39e3c5366c3717adc1d2f45bf44c62bec0817f
Micah Smith <micahjsmith@gmail.com>
2018-02-12 16:51:26 -0500

    Update

--- submodules.sh
+++ submodules.sh
@@ -2,3 +2,9 @@
 
 git submodule init
 git submodule update
+
+# To update Flex repo
+# cd themes/Flex
+# git fetch --tags
+# latest_tag = $(git describe --tags `git rev-list --tags --max-count=1`)
+# git checkout $latest_tag
